0e9e33c9e8d154985e5d08478900918f9dfe1d2d
maybemagenta <76878023+maybemagenta@users.noreply.github.com>
2021-04-02 17:30:12 -0600

    Create quillflight.php

---
+++ quillflight.php
@@ -0,0 +1,70 @@
+
+
+      /*  by #35127  */
+
+
+* { font-family: 'calibri', sans-serif;
+font-size: inherit; color: inherit; border: 0 }
+
+#footer, #sidebar, .card.text-center .card-img-top, .breadcrumb-item { display: none }
+#main.col-lg-9 { max-width: 100%; flex: 1 }
+#main::after { content: "css by ᓚᘏᗢ #35127"; white-space: pre-wrap;font-size: 80%; display: block; text-align: right; margin-bottom: 1em; line-height: 1; }
+#header .breadcrumb-nav .breadcrumb { margin: 0; max-width: 100% }
+.table a { font-weight: inherit }
+.table .left { background-color: inherit }
+.table td, .table th { vertical-align: middle }
+body { cursor: default }
+#header .navbar-main .nav-link .fa { color: inherit }
+nav i, .nav-tabs { display: none!important }
+br { display: none }
+
+.navbar-status { display: none } @media (min-width: 1200px) { #header .navbar-main { height: 105px; padding-left: 300px } }
+
+form td:nth-child(2) { width: 10% }
+form td:nth-child(3) { width: 25% }
+form td:nth-child(4) { width: 10% }
+form td:nth-child(5) { width: 7% }
+
+
+
+.mb-3, .my-3 { margin: 0 auto }.table td {
+    align-items: center;
+}
+
+.featured-wolf b { display: block }
+
+td.b { width: 35% }
+
+.form-row .card.text-center { margin-top: 1rem }
+
+
+
+
+/* headers */
+#header .breadcrumb-nav { background-color: transparent }
+
+/* colored */
+h1 { font-family: 'Kristi', cursive; text-align: center }
+body { background-image: url('https://images2.imgbox.com/34/56/uRls0qdh_o.png'); background-size: auto; background-repeat: repeat; background-attachment: fixed;
+color: #000 }
+a { color: pink }
+#main a { color: #ddd }
+
+#header .navbar-main,
+#main {
+    background: #ffffffd6;
+    color: #818181;
+    border: 1px solid #81818163;
+}
+
+a, a.text-danger, .nav-link, .table th a, .alert-info .alert-link, .alert-success .alert-link
+{ color: #81818163; transition: .5s; text-decoration: none }
+
+a:hover, a.text-danger:hover, #header .navbar-status a:hover, .nav-link:hover, #footer nav .nav-item .nav-link:hover, #header .navbar-main .active .nav-link, #header .navbar-main .active .nav-link .fa, #header .navbar-main .nav-link:hover, #header .navbar-main .nav-link:hover .fa
+{ color: #000; text-decoration: none }
+
+
+
+#header .navbar-main .nav-link {
+    color: #b2b2b2;
+}
